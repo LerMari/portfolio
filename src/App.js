1a26d960c85b45b12e31f1469ed22a1afbd8a060
Marisa <lermamarisa@gmail.com>
2023-03-10 21:04:03 -0800

    worked on nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <NavBar />
+      <div id="name">Marisa<br></br>
+      Lerma</div>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div id="navlinks">
+        <h1>WEB</h1>
+        <h1>GRAPHIC DESIGN</h1>
+        <h1>ABOUT</h1>
+        <h1>CONTACT</h1>
+    </div>
+  )
+}
+
+export default NavBar
