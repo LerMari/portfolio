ffd0f39fbf015c039e350031251521a35e66ace9
Marisa <lermamarisa@gmail.com>
2023-06-24 19:07:04 -0700

    finished modal functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import Modal from './Components/Modal/Modal';
+import {useState} from 'react';
 
 
 function App() {
+const [openModal, setOpenModal] = useState(false)
+
   return (
     <div className="App">
       <NavBar />
-      <Modal />
+      <button className="openModalBtn" onClick={() => {setOpenModal(true)}}>Open</button>
+      {openModal && <Modal closeModal={setOpenModal} />}
       <div id="name">Marisa<br></br>
       Lerma</div>
     </div>
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Modal.css';
 
-function Modal() {
+function Modal({closeModal}) {
   return (
-    <div className="modal-background">
-        <div className="mondal-container"></div>
-            <button>X</button>
+    <div className="modalBackground">
+        <div className="modalContainer"></div>
+            <div className="titleCloseBtn">
+              <button onClick={() => closeModal(false)}>X</button>
+              </div>
             <div className="title"><h1>Hello!</h1></div>
             <div className="body"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
-            <div className="footer"><button>Close</button></div>
+            <div className="footer"><button onClick={() => closeModal(false)}>Close</button></div>
 
         Modal</div>
   )
